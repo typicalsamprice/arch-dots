0c8821f5a7001f9becb406b6ac9fa6731ca01bdd
Sam Price <sam@typicalsamprice.com>
2024-04-22 12:36:31 -0500

    [emacs]  Revamp config
    Change font
    Change theme
    Add tex commands/options for QOL

--- config-folder-dots/doom/config.el
+++ config-folder-dots/doom/config.el
@@ -1,6 +1,19 @@
 (setq user-name "Sam Price"
       user-email "sam@typicalsamprice.com")
 
-(setq catppucin-flavor 'frappe)
+(setq doom-font (font-spec :family "GeistMono Nerd Font" :size 18))
+
+(setq catppucin-flavor 'latte)
 (load-theme 'catppuccin t t)
 (catppuccin-reload)
+
+(add-to-list 'tex--prettify-symbols-alist '("\\eps" . 1013))
+
+;; Make doing this so nice and easy
+(add-to-list 'display-buffer-alist (cons "\\*Async Shell Command\\*.*" (cons #'display-buffer-no-window nil)))
+
+(setq async-shell-command-buffer #'new-buffer)
+
+(defun open-zathura-texpdf ()
+  (interactive)
+  (async-shell-command (concat "zathura " (file-name-sans-extension (buffer-name)) ".pdf")))
